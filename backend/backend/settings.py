51efeb93818c9c9b3925405aca048b489ecfb767
Adrian Domański <adrian.domanski.dev@gmail.com>
2026-04-10 19:11:15 +0200

    fix: static url

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -135,10 +135,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+STATIC_ROOT = "/app/staticfiles"
+MEDIA_ROOT = "/app/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
